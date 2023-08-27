f596a624f5f397d5ca9589ba553fb72af9a5ab43
LeDucNghi <81682075+LeDucNghi@users.noreply.github.com>
2023-08-27 10:07:25 +0700

    update BE

--- server/src/api/emojis.js
+++ server/src/api/emojis.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json(['😀', '😳', '🙄']);
+// router.get('/', (req, res) => {
+//   res.json(['😀', '😳', '🙄']);
+// });
+
+router.get("/", async (req, res) => {
+  const { username, password } = req.body;
+
+  const respsonse = await UserModel.findOne({ username }).select("-password");
+
+  return res.json({ ...respsonse });
 });
 
 module.exports = router;
