304e0f7dc96c683fc637f36719f367a77dfb3d75
LeDucNghi <81682075+LeDucNghi@users.noreply.github.com>
2023-09-25 10:57:36 +0700

    update code

--- server/src/app.js
+++ server/src/app.js
@@ -16,7 +16,11 @@ const io = socket(server, {
 });
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
