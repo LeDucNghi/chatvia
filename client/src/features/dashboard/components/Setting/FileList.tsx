8b50fb47921cf01dc891ccc34f3af3a0a9e792dc
LeDucNghi <81682075+LeDucNghi@users.noreply.github.com>
2023-12-14 09:11:54 +0700

    update setting UI

--- client/src/features/dashboard/components/Setting/FileList.tsx
+++ client/src/features/dashboard/components/Setting/FileList.tsx
@@ -1,4 +1,5 @@
 import DownloadIcon from "@mui/icons-material/Download";
+import { IconButton } from "@mui/material";
 import { Images } from "../../../../constants";
 import { files } from "../../../../mock";
 
@@ -6,16 +7,16 @@ export interface IFileListProps {}
 
 export function FileList() {
   return (
-    <div className="p-4">
+    <div className="py-4 px-2">
       <div className="flex items-center flex-col cursor-pointer">
         {files.slice(0, 3).map((file, key) => {
           return (
             <div
               key={key}
-              className="w-4/5 flex justify-between py-2 mb-4 hover-gray rounded-lg"
+              className="w-4/5 flex justify-between py-2 px-2 mb-4 hover-gray rounded-lg"
               // onClick={() => handleSelectImage(user.avatar!)}
             >
-              <div className="w-[30px] h-10 bg-gray-300 flex-center rounded-lg">
+              <div className="w-[30px] mr-1 h-10 bg-gray-300 flex-center rounded-lg">
                 <img
                   className="w-2/5 h-full object-contain rounded-xl"
                   src={Images.document}
@@ -32,9 +33,11 @@ export function FileList() {
                 </div>
               </div>
 
-              <div className="">
+              <div className="h-full ">
                 <a href={Images.attachments} download="Attachment.png">
-                  <DownloadIcon />
+                  <IconButton>
+                    <DownloadIcon />
+                  </IconButton>
                 </a>
               </div>
             </div>
--- client/src/features/dashboard/components/Setting/LinksModal.tsx
+++ client/src/features/dashboard/components/Setting/LinksModal.tsx
@@ -1,4 +1,3 @@
-import DownloadIcon from "@mui/icons-material/Download";
 import { Images } from "../../../../constants";
 import { files } from "../../../../mock";
 
@@ -6,13 +5,16 @@ export interface ILinksModalProps {}
 
 export function LinksModal(props: ILinksModalProps) {
   return (
-    <div className="p-4">
-      <div className="flex items-center flex-col cursor-pointer">
+    <div className="py-4 px-2">
+      <div className="flex items-center flex-col cursor-pointer ">
         {files.slice(0, 3).map((file, key) => {
           return (
-            <div
+            <a
               key={key}
-              className="w-4/5 flex justify-between py-2 mb-4 hover-gray rounded-lg"
+              href="https://www.facebook.com/nghile.genji/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-4/5 flex justify-between py-2 px-6 mb-4 hover-gray rounded-lg"
               // onClick={() => handleSelectImage(user.avatar!)}
             >
               <div className="w-[30px] h-10 bg-gray-300 flex-center rounded-lg">
@@ -27,17 +29,10 @@ export function LinksModal(props: ILinksModalProps) {
                 <p className="font-bold w-full truncate text-xs">{file.name}</p>
 
                 <div className="w-full flex justify-between text-xs font-semibold text-gray-400">
-                  {/* <p> {file.length}MB </p> */}
                   <p> {file.date} </p>
                 </div>
               </div>
-
-              <div className="">
-                <a href={Images.attachments} download="Attachment.png">
-                  <DownloadIcon />
-                </a>
-              </div>
-            </div>
+            </a>
           );
         })}
       </div>
