7361d27e6856529258a2c5bab8794e7d58c785a2
LeDucNghi <81682075+LeDucNghi@users.noreply.github.com>
2024-03-02 22:17:12 +0700

    udpate code

--- client/src/features/dashboard/dashboardThunk.ts
+++ client/src/features/dashboard/dashboardThunk.ts
@@ -30,29 +30,28 @@ import { socket } from "../../constants";
 export const sendMsg =
   (values: Message): AppThunk =>
   async (dispatch, getState) => {
-    console.log("🚀 ~ values:", values);
     try {
       const user = getState().auth.user;
 
-      // socket.emit("send-message", {
-      //   user,
-      //   ...values,
-      // });
+      socket.emit("send-message", {
+        user,
+        ...values,
+      });
 
-      // socket.emit("notifications", {
-      //   room: values.consId,
-      //   user: user,
-      //   content: `${user?.username} has sent to you a message`,
-      //   type: "newMsg",
-      // });
+      socket.emit("notifications", {
+        room: values.consId,
+        user: user,
+        content: `${user?.username} has sent to you a message`,
+        type: "newMsg",
+      });
 
-      // dispatch(
-      //   addNewMessage({
-      //     consId: values.consId,
-      //     message: values.message,
-      //     sender: user!,
-      //   })
-      // );
+      dispatch(
+        addNewMessage({
+          consId: values.consId,
+          message: values.message,
+          sender: user!,
+        })
+      );
     } catch (error) {
       console.log("🚀 ~ file: dashboardThunk.ts:9 ~ sendMsg ~ error:", error);
       alert({
